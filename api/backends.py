c8d0a586b4574402b795a0dad8d9d52bbd6969a1
Md. Mahfuz Bhuiyan <codomahfuz@gmail.com>
2025-10-01 19:25:22 +0600

    1st

---
+++ api/backends.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.backends import ModelBackend
+from .models import User
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, email=None, password=None, **kwargs):
+        if email is None or password is None:
+            return None
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
